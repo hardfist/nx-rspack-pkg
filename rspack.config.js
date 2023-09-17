fda347bc7a81205d4ff5149970405ccbe5a65eac
hardfist <yangjianzju@gmail.com>
2023-09-17 12:46:14 +0800

    chore: add generate json

--- rspack.config.js
+++ rspack.config.js
@@ -1,5 +1,16 @@
 const { composePlugins, withNx, withReact } = require('@nx/rspack');
+const GeneratePackageJsonPlugin = require('generate-package-json-webpack-plugin');
 
 module.exports = composePlugins(withNx(), withReact(), (config) => {
+  config.plugins.push(
+    new GeneratePackageJsonPlugin({
+      name: 'my-nodejs-module',
+      version: '1.0.0',
+      main: './index.js',
+      engines: {
+        node: '>= 14',
+      },
+    })
+  );
   return config;
 });
